99d417e225c6d0c617001534e5bde572f01ae554
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-07 22:17:27 +0545

    minor changes to files

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
-const counter = useSelector((state) => state.counter);
-  return (
-    <p className="lead mb-4">
-    Counter current Value {counter}
-    </p>
-  );
+  /*
+  // counter is the name of our slice
+  const counterObj = useSelector((state) => state.counter);
+  const counter = counterObj.counterVal;
+*/
+  const { counterVal } = useSelector((store) => store.counter);
+  return <p className="lead mb-4">Counter current Value {counterVal}</p>;
 };
 
 export default DisplayCounter;
